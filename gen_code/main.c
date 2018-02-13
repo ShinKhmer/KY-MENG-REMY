94730c1a62e6beadc5fc15501af78b5a3227c276
Shevraar <sky050577@gmail.com>
2018-02-13 21:18:56 +0100

    log_in_out +verf + début sign_in

--- gen_code/main.c
+++ gen_code/main.c
@@ -8,17 +8,27 @@
 
 void generate_code(char* code);
 int check_code(char *code_string);
+int log_in_out(int *id_location, char * code_customer);
+
 
 
 int main(int argc, char ** argv)
 {
-
+    /** ==== PART CODE GENERATOR =====   **/
+    /*
     char code[11]="";
     do{
             generate_code(code);
     }while(check_code(code));
     printf("client code : %s\n",code);
+    */
+    /** ==== PART INSERT DATABASE ===== **/
 
+
+
+
+
+    printf("plop\n");
     return 0;
 }
 
@@ -66,6 +76,7 @@ int check_code(char *code_string){
             }
         }
         mysql_free_result(result);
+        mysql_close(mysql);
         return 0;
     }
     else {
@@ -74,3 +85,79 @@ int check_code(char *code_string){
     return 99;
 }
 
+int log_in_out(int *id_location, char * code_customer){
+
+    int id_customer = 0;
+    int inside;
+
+    MYSQL *mysql;
+    MYSQL_RES *result = NULL;
+    MYSQL_ROW row;
+    char request[150];
+
+    mysql = mysql_init(NULL);
+    mysql_options(mysql, MYSQL_READ_DEFAULT_GROUP, "option");
+
+    if(mysql_real_connect(mysql, "127.0.0.1", "root","", "worknshare", 0, NULL, 0))
+    {
+        sprintf(request,"SELECT * FROM customer");
+        mysql_query(mysql,request);
+        result = mysql_use_result(mysql);
+        while ((row = mysql_fetch_row(result)))
+        {
+            if(strcmp(row[5],code_customer)== 0)
+            {
+                id_customer = row[0];
+                inside = row[6];
+            }
+        }
+        mysql_free_result(result);
+        if(id_customer != 0 && inside = 0)
+        {
+            sprintf(request,"INSERT INTO `history`(`id_history`, `date_entry`, `date_exit`, `id_customer`, `id_location`) VALUES (NULL,[value-2],NULL,%d,%d)",id_customer,id_location);
+            mysql_query(mysql,request);
+            sprintf(request,"UPDATE `customer` SET inside = 1 WHERE id_customer = %d",id_customer);
+            mysql_query(mysql,request);
+
+        }
+        else if(id_customer != 0 && inside = 1)
+        {
+
+            sprintf(request,"UPDATE `history` SET `date_exit` = [value] WHERE id_customer = %d AND date_exit = NULL",id_customer);
+            mysql_query(mysql,request);
+            sprintf(request,"UPDATE `customer` SET inside = 0 WHERE id_customer = %d",id_customer);
+            mysql_query(mysql,request);
+        }
+        else
+        {
+            mysql_close(mysql);
+            return 99;
+        }
+        mysql_close(mysql);
+    }
+    else {
+            return 99;
+    }
+    return 99;
+}
+
+
+void sign_in(){
+    MYSQL *mysql;
+    MYSQL_RES *result = NULL;
+    MYSQL_ROW row;
+    char request[150];
+
+    mysql = mysql_init(NULL);
+    mysql_options(mysql, MYSQL_READ_DEFAULT_GROUP, "option");
+
+    if(mysql_real_connect(mysql, "127.0.0.1", "root","", "worknshare", 0, NULL, 0))
+    {
+        sprintf(request,"INSERT INTO `customer`( `name_customer`, `surname_customer`, `phone_number`, `email`, `code_customer`, `inside`, `id_subscription`) VALUES ([value-2],[value-3],[value-4],[value-5],[value-6],0,0)");
+        mysql_query(mysql,request);
+    }
+    else {
+            return 99;
+    }
+    return 99;
+}
